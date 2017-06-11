71be5968fe94f514042d65758e8af8c3de77a535
Baptiste Becmeur <baptistebecmeur@gmail.com>
2017-06-11 17:12:24 +0200

    modification controller

--- app/controllers/prestations_controller.rb
+++ app/controllers/prestations_controller.rb
@@ -2,13 +2,14 @@ class PrestationsController < ApplicationController
 
 before_action :set_prestation, only: [:show, :edit, :update]
 before_action :authenticate_user!, except: [:show]
+before_action :require_same_user, only: [:edit, :update]
   def index
     @prestations = current_user.prestations
   end
 
-  # def show
-  #   @prestation = Prestation.find(params[:id])
-  # end
+  def show
+    @photos = @prestation.photos
+  end
 
   def new
     @prestation = current_user.prestations.build
@@ -17,19 +18,31 @@ before_action :authenticate_user!, except: [:show]
   def create
     @prestation = current_user.prestations.build(prestation_params)
     if @prestation.save
-      redirect_to @prestation, notice:"Votre prestation a été ajouté avec succès"
+      if params[:images]
+          params[:images].each do |i|
+            @prestation.photos.create(image: i)
+        end
+      end
+      @photos = @prestation.photos
+      redirect_to edit_prestation_path(@prestation), notice:"Votre prestation a été ajouté avec succès"
     else
       render :new
     end
   end
 
-  # def edit
-  #   @prestation = Prestation.find(params[:id])
-  # end
+  def edit
+    @photos = @room.photos
+  end
 
   def update
     if prestation.update(prestation_params)
-      redirect_to @prestation, notice: "Modification enregistrée"
+       if params[:images]
+          params[:images].each do |i|
+            @prestation.photos.create(image: i)
+        end
+      end
+      @photos = @prestation.photos
+      redirect_to edit_prestation_path(@prestation), notice: "Modification enregistrée"
     else
       render :edit
     end
@@ -45,5 +58,12 @@ before_action :authenticate_user!, except: [:show]
 
     def prestation_params
      params.require(:prestation).permit(:talent, :showing_type, :duration, :zone_km, :name_scene, :listing_name, :summary, :address, :zip_code, :city, :country, :is_equipment, :is_indoor, :price, :active )
+    end
+
+    def require_same_user
+      if current_user.id != @room.user_id
+        flash[:danger] = "Vous n'avez pas le droit de modifier cette page"
+        redirect_to root_path
+      end
     end
 end
