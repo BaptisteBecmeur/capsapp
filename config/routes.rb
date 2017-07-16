f2a226ecceaaa14706c8073313c355f6bf93472f
Baptiste Becmeur <baptistebecmeur@gmail.com>
2017-07-16 21:24:10 +0200

    listes prestas profile done

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   get '/preload' => 'reservations#preload'
   get '/preview' => 'reservations#preview'
-  get 'your_books' => 'reservations#your_books', path: 'mes-reservations'
+  get 'your_books' => 'reservations#your_books', path: 'mes-transactions'
   get '/your_reservations' => 'reservations#your_reservations', path: 'suivi-des-reservations'
 
 
