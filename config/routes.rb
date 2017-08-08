449eadef1f4f69d4c5a9422f0d6385b7906b6364
Baptiste Becmeur <baptistebecmeur@gmail.com>
2017-08-08 14:37:00 +0200

    conversation + messages controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   devise_for :users, :path=>'',
                      :path_names=>{:sign_in=>'login', :sign_out=>'logout', :edit=>'profile'},
                      :controllers=>{:registrations=>'registrations'}
@@ -19,6 +18,10 @@ Rails.application.routes.draw do
   get '/your_reservations' => 'reservations#your_reservations', path: 'suivi-des-reservations'
 
   get'/search' => 'pages#search'
+
+  resources :conversations, only: [:index, :create] do
+    resources :messages, only: [:index, :create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
