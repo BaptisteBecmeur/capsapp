fbed9868d5c7391a9275e6d714408ff3d18c72f2
Baptiste Becmeur <baptistebecmeur@gmail.com>
2017-08-08 17:44:00 +0200

    AJAX pour messages done

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,21 +4,23 @@ class MessagesController < ApplicationController
     before_action :set_conversation
 
     def index
-        if current_user == @conversation.sender || current_user == @conversation.recipient
-            @other = current_user == @conversation.sender ? @conversation.recipient : @conversation.sender
-            @messages = @conversation.messages.order("created_at DESC")
-        else
-            redirect_to conversations_path, alert: "Vous ne pouvez pas accéder à cette page."
-        end
+      if current_user == @conversation.sender || current_user == @conversation.recipient
+        @other = current_user == @conversation.sender ? @conversation.recipient : @conversation.sender
+        @messages = @conversation.messages.order("created_at DESC")
+      else
+        redirect_to conversations_path, alert: "Vous ne pouvez pas accéder à cette page."
+      end
     end
 
     def create
-        @message = @conversation.messages.new(message_params)
-        @messages = @conversation.messages.order("created_at DESC")
+      @message = @conversation.messages.new(message_params)
+      @messages = @conversation.messages.order("created_at DESC")
 
         if @message.save
-           redirect_to conversation_messages_path(@conversation)
+          respond_to do |format|
+            format.js
         end
+      end
     end
 
 
