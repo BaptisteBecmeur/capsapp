bfc9c32ccfe9ad86f850c6b0f343c135b9116752
Florent Allain <allain.florent@gmail.com>
2017-08-21 23:07:52 +0200

    feat(ransack): Initiate basic search from homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,40 +1,16 @@
 class PagesController < ApplicationController
   def home
+    @q = Prestation.active.ransack(params[:q])
     @prestations = Prestation.order("RANDOM()").limit(9)
   end
 
   def search
-    if params[:search].present? && params[:search].strip != ""
-      session[:presta_search] = params[:search]
-    end
-
-    arrResult = Array.new
-
-    if session[:presta_search] && session[:presta_search] != ""
-      @prestations_address = Prestation.where(active: true).near(session[:presta_search], 5, order:'distance')
-    else
-      @prestations_address = Prestation.where(active: true).all
-    end
-
-    @search = @prestations_address.ransack(params[:q])
-    @prestations = @search.result
-
-    @arrprestations = @prestations.to_a
-
-    if (params[:start_date] && params[:end_date] && !params[:start_date].empty? & !params[:end_date].empty?)
-      start_date = Date.parse(params[:start_date])
-      end_date = Date.parse(params[:end_date])
-
-      @prestations.each do |prestation|
-       not_available = prestation.reservations.where("(? <= start_date AND start_date <= ?) OR
-            (? <= end_date AND end_date <= ?) OR (start_date < ? AND ? < end_date)", start_date, end_date,
-            start_date, end_date, start_date, end_date).limit(1)
-
-        if not_available.length > 0
-        @arrprestations.delete(prestation)
-        end
+    @q = Prestation.active.ransack(params[:q])
+    @prestations =
+      if @q.result.any?
+        @q.result.includes(:reservations)
+      else
+        Prestation.active
       end
-    end
   end
-
 end
