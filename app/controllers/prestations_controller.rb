2a81ad5f4f931c0b151afe7894af8645a42eb79a
Baptiste Becmeur <baptistebecmeur@gmail.com>
2017-08-27 22:03:39 +0200

    ok

--- app/controllers/prestations_controller.rb
+++ app/controllers/prestations_controller.rb
@@ -12,8 +12,9 @@ before_action :require_same_user, only: [:edit, :update]
 
     @photos = @prestation.photos
     @booked = Reservation.where("prestation_id = ? AND user_id = ?", @prestation.id, current_user.id).present?
-    if current_user
+
       @reviews = @prestation.reviews
+    if current_user
       @hasReview = @reviews.find_by(user_id: current_user.id) if current_user
     end
 
